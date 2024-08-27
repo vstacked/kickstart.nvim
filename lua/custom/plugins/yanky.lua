f0d5a95a5d4591bc03a682eb60635a9521627eca
rizki <54944887+vstacked@users.noreply.github.com>
2024-08-27 22:45:11 +0700

    feat: update yanky conf & snippet

--- lua/custom/plugins/yanky.lua
+++ lua/custom/plugins/yanky.lua
@@ -1,24 +1,22 @@
 return {
   {
     'gbprod/yanky.nvim',
+    dependencies = {
+      { 'kkharji/sqlite.lua' },
+    },
     event = 'VeryLazy',
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      ring = { storage = 'sqlite' },
     },
-    config = function()
-      require('yanky').setup()
-
-      -- vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
-      -- vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
-      -- vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)')
-      -- vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)')
-      --
-      -- vim.keymap.set('n', '<c-p>', '<Plug>(YankyPreviousEntry)')
-      -- vim.keymap.set('n', '<c-n>', '<Plug>(YankyNextEntry)')
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>sy', '<cmd>Telescope yank_history<cr>', { desc = 'Yank History' })
-    end,
+    keys = {
+      {
+        '<leader>sy',
+        function()
+          require('telescope').extensions.yank_history.yank_history {}
+        end,
+        mode = { 'n', 'x' },
+        desc = 'Open Yank History',
+      },
+    },
   },
 }
